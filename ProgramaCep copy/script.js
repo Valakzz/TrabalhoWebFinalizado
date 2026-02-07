0965108cf99e8a3f7a8f91cc353c06c8d2a00394
Valakzz <chucklord14@gmail.com>
2026-02-07 13:09:21 -0300

    Add files via upload

---
+++ ProgramaCep copy/script.js
@@ -0,0 +1,17 @@
+
+
+const cepbusq = document.getElementById("cepbusqq");
+const liimpar = document.getElementById("limpcep")
+
+function searchCep(){
+    
+    alert("Buscando CEP...");
+}
+
+function limparCep(){
+    alert("Limpando campo...");
+}
+
+cepbusq.addEventListener("click",searchCep)
+liimpar.addEventListener("click",limparCep)
+
